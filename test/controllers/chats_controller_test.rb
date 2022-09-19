de21a1ed9950a64e8dd572810dd10d92f97b2c24
Seaanna <seaanna.thompson@gmail.com>
2022-09-19 08:04:17 -0700

    add model and controller tests

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -16,11 +16,52 @@ class ChatsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response :created
+    assert_equal @response.status, 201
+    assert @response.body.include?('id')
+    assert !@response.body.include?('username')
   end
 
   test "should show chat" do
     get chat_url(@chat), as: :json
     assert_response :success
+
+    assert_equal @response.status, 200
+    assert !@response.body.include?('id')
+    assert @response.body.include?('username')
+    assert @response.body.include?('text')
+    assert @response.body.include?('expiration_date')
+    assert @response.body.include?('is_read')
+  end
+
+  test "should get chat by username" do
+    @chat = Chat.create(username: "sea123", text: "testing")
+
+    get chats_by_username_url('sea123'), as: :json
+    assert_response :success
+
+    assert_equal @response.status, 200
+    assert @response.body.include?('id')
+    assert @response.body.include?('text')
+    assert @response.body.include?('is_read')
+    assert !@response.body.include?('username')
+  end
+
+  test "should not get expired messages for chats by username" do
+    @chat = Chat.create(username: "sea123", text: "testing", timeout: 0)
+
+    get chats_by_username_url('sea123'), as: :json
+    assert_response :success
+
+    assert_equal @response.body, '[]'
+  end
+
+  test "should not get read messages for chats by username" do
+    @chat = Chat.create(username: "sea123", text: "testing", is_read: true)
+
+    get chats_by_username_url('sea123'), as: :json
+    assert_response :success
+
+    assert_equal @response.body, '[]'
   end
 
   test "should update chat" do
--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,7 +1,80 @@
 require "test_helper"
 
 class ChatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @chat_blank = Chat.create
+    @chat_filled = Chat.create(
+      username: "sea123",
+      text: "testing is fun",
+    )
+    @chat_filled_2 = Chat.create(
+      username: "sea123",
+      text: "another chat",
+    )
+    @chat_filled_3 = Chat.create(
+      username: "sea123",
+      text: "another chat",
+      timeout: 120
+    )
+  end
+
+  test "username is required" do
+    assert @chat_blank.errors.full_messages.include?("Username can't be blank")
+  end
+
+  test "text is required" do
+    assert @chat_blank.errors.full_messages.include?("Text can't be blank")
+  end
+
+  test "saves with valid fields" do
+    assert @chat_filled.valid?
+    assert @chat_filled.id.present?
+  end
+
+  test "expiration_date defaults to 60 seconds" do
+    assert_equal @chat_filled_2.expiration_date, @chat_filled_2.created_at + 60.seconds
+  end
+
+  test "expiration_date is set to created_at + timeout" do
+    assert_equal @chat_filled_3.expiration_date, @chat_filled_3.created_at + 120.seconds
+  end
+
+  test "json for index format" do
+    json = @chat_filled.as_json({index: true})
+
+    assert_equal json[:username], "sea123"
+    assert_equal json[:text], "testing is fun"
+    assert json[:expiration_date]
+    assert_equal json[:is_read], false
+    assert !json[:id]
+  end
+
+  test "json for username format" do
+    json = @chat_filled.as_json({username: true})
+
+    assert json[:id]
+    assert_equal json[:text], "testing is fun"
+    assert_equal json[:is_read], false
+    assert !json[:username]
+    assert !json[:expiration_date]
+  end
+
+  test "json for show format" do
+    json = @chat_filled.as_json({show: true})
+
+    assert_equal json[:username], "sea123"
+    assert_equal json[:text], "testing is fun"
+    assert json[:expiration_date]
+    assert_equal json[:is_read], false
+    assert !json[:id]
+  end
+
+  test "json for create format" do
+    json = @chat_filled.as_json({create: true})
+    assert json[:id]
+    assert !json[:username]
+    assert !json[:text]
+    assert !json[:expiration_date]
+    assert !json[:is_read]
+  end
 end
